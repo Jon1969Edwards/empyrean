0eccc6b63164ba231f677d6cf116ac0812a4c78f
Jon1969Edwards <jon1969edwards@gmail.com>
2021-05-10 13:13:08 +0200

    Adds logo and welcome sign

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './eclanding.png';
+import logo from './images/eclanding.png';
+import welcome from './images/welcomeSign.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
-        <br></br>
-        <a
-          className="App-link"
-          href="http://www.empyrean.meleeconvergent.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Empyrean Casino
-        </a>
-      </header>
-    </div>
+    // <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            
+          </p>
+          <a
+            className="App-link"
+            href="http://www.empyrean.meleeconvergent.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={welcome} className="App-link" alt="welcome" />
+          </a>
+        </header>
+      </div>
+    // </div>
   );
 }
 
